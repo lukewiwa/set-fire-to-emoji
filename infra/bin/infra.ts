526f826cdd91917e89a481f483dbb9338f04a4f0
lukewiwa <luke.wiwa@gmail.com>
2022-08-16 10:57:59 +0000

    Update stack name

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,7 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
 const app = new cdk.App();
-new InfraStack(app, "InfraStack", {
+new InfraStack(app, "SetFireInfraStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
